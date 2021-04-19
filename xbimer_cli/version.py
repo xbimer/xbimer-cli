136f265e99b044ec551d3134164326f31787c1f0
beanjs <502554248@qq.com>
2021-04-19 20:39:58 +0800

    1.change fg='yellow'

--- xbimer_cli/version.py
+++ xbimer_cli/version.py
@@ -20,7 +20,7 @@ def updateCheck():
     if compare(LATEST_VERSION, CURRENT_VERSION) == 1:
         secho(
             f'WARNING: You are using xbimer-cli version {CURRENT_VERSION}, however version {LATEST_VERSION} is available.',
-            fg='green')
+            fg='yellow')
         secho(
             f'You should consider upgrading via the \'pip install --upgrade xbimer-cli\' command.',
-            fg='green')
+            fg='yellow')
